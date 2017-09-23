9de0d843b41189cd3ce450eb08532b6cddddf9cb
nikita <nikitajz17@gmail.com>
2017-09-23 10:04:06 +0300

    remove leftovers from testing

--- scrape03online.py
+++ scrape03online.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 # import pandas as pd
+import json
 from pprint import pprint
 import requests
 import re
@@ -20,11 +21,13 @@ soup = BeautifulSoup(rm.text, 'html.parser')
 
 doctors = dict()
 
+# collect a link for each doctor/group
 for a in soup.find_all('a', href=re.compile("/news/\w+/1-0-[0-9]+")):
     # print(a)
     doctors[a.text] = {
         'link': the_url + a['href'],
         'max_page': 1,
+        'question_links': [],
         'questions': []
         }
 
@@ -57,10 +60,11 @@ def get_max_question_block_page(links):
     return max_page
 
 
-d1 = {'Аллерголог': {'link': the_url + '/news/allergolog/1-0-23'}}  # for testing only
+# d1 = {'Аллерголог': {'link': the_url + '/news/allergolog/1-0-23'}}  # for testing only
 
-# for d, v in doctors.items(): # replace below line when testing completed
-for (d, v) in d1.items():
+# get latest question block page
+# for (d, v) in d1.items():  # purely for testing
+for d, v in doctors.items():  # replace near line when testing completed
     rp = requests.get(v['link'])  # request question blocks page
     sp = BeautifulSoup(rp.text, 'html.parser')
     v['max_page'] = get_max_question_block_page(sp.select('div[class="paging-block"]')[0].select('a[href]'))
@@ -88,8 +92,9 @@ def get_question_links(link, max_page, timeout=0.25):
     For example:
         link: http://03online.com/news/allergolog/1-0-23
         max_page: 457
-    :param link: string
-    :param max_page: int
+    :param link[string]
+    :param max_page[int]
+    :param timeout[int] between retries
     :return list[question_link]
     """
 
@@ -105,7 +110,7 @@ def get_question_links(link, max_page, timeout=0.25):
         return links
 
     question_links = []
-    for p in range(1, max_page):
+    for p in range(1, max_page + 1):
         u = link.replace('/1-', '/{}-'.format(p))
         print(u)
         rq = requests.get(u)  # request questions block page
@@ -117,8 +122,8 @@ def get_question_links(link, max_page, timeout=0.25):
 
 
 # collect questions urls from block pages
-# for d, v in doctors.items(): # replace below line when testing completed
-for d, v in d1.items():
+# for d, v in d1.items(): # purely for testing
+for d, v in doctors.items():  # replace below line when testing completed
     v['question_links'] = get_question_links(v['link'], v['max_page'])
     doctors[d] = v
 
@@ -126,14 +131,19 @@ for d, v in d1.items():
 for d, v in doctors.items():
     questions = []
     for l in v['question_links']:
+        print("Collection questions for page", l)
         question_tags = soup.select("#content_main > div.divide-block.question-block")[0]
         q = Question(question_tags)
         questions.extend(q)
         v['questions'] = questions
         doctors[d] = v
+        time.sleep(0.25)
 
 
 pprint(doctors['Аллерголог'])
 
 # TODO: add throttling
 # TODO: add export to file (json?)
+
+with open('data2.txt', 'w') as outfile:
+    json.dump(doctors, outfile)
